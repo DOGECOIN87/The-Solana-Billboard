d59bc3614e15297275970ae30c9ca9a6365b2e0d
Mattrick <82479263+DOGECOIN87@users.noreply.github.com>
2024-09-17 22:23:30 -0500

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,8 +5,9 @@ const MenuContainer = styled.div`
   position: absolute;
   right: 0;
   top: 0;
-  width: 200px;
-  background: rgba(0,0,0,0.5);
+  width: 250px;
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -14,17 +15,32 @@ const MenuContainer = styled.div`
 `;
 
 function Menu() {
+  const handleUploadMedia = () => {
+    // Implement media upload functionality
+  };
+
+  const handleAddText = () => {
+    // Implement text input functionality
+  };
+
+  const handleColorEffects = () => {
+    // Implement color and effects functionality
+  };
+
+  const handleSaveToBlockchain = () => {
+    // Implement save to blockchain functionality
+  };
+
   return (
     <MenuContainer>
       <h2>The Solana Billboard Menu</h2>
       <button>Draw</button>
-      <button>Upload Media</button>
-      <button>Text Input</button>
-      <button>Color & Effects</button>
-      <button>Save to Blockchain</button>
+      <button onClick={handleUploadMedia}>Upload Media</button>
+      <button onClick={handleAddText}>Text Input</button>
+      <button onClick={handleColorEffects}>Color & Effects</button>
+      <button onClick={handleSaveToBlockchain}>Save to Blockchain</button>
     </MenuContainer>
   );
 }
 
 export default Menu;
-
