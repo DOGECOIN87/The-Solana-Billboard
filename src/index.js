33691a12a225b6d2103bda51477a2e04ffde12dd
Mattrick <82479263+DOGECOIN87@users.noreply.github.com>
2024-09-17 22:32:53 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
