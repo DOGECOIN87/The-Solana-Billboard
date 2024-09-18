2e34315088b1ffbff2fa787ef068f5935104cc1c
Mattrick <82479263+DOGECOIN87@users.noreply.github.com>
2024-09-17 22:22:21 -0500

    Update Scene.js

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,24 +1,29 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Loader } from '@react-three/drei';
 import Billboard from './Billboard';
 import Highway from './Highway';
 import Cars from './Cars';
 
 function Scene() {
   return (
-    <Canvas camera={{ position: [0, 5, 10], fov: 60 }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 10, 5]} intensity={1} />
-      <Suspense fallback={null}>
-        <Billboard />
-        <Highway />
-        <Cars />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+    <>
+      <Canvas
+        camera={{ position: [0, 5, 15], fov: 60 }}
+        style={{ height: '100vh', width: '100%' }}
+      >
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 10, 5]} intensity={1} />
+        <Suspense fallback={null}>
+          <Billboard />
+          <Highway />
+          <Cars />
+        </Suspense>
+        <OrbitControls enablePan={false} maxPolarAngle={Math.PI / 2} />
+      </Canvas>
+      <Loader />
+    </>
   );
 }
 
 export default Scene;
-
