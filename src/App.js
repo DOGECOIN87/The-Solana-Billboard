f2544fbb2272fe4c41fce1b2463b418996980417
Mattrick <82479263+DOGECOIN87@users.noreply.github.com>
2024-09-17 22:18:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,25 +10,40 @@ import {
 } from '@solana/wallet-adapter-react-ui';
 import {
   PhantomWalletAdapter,
-  SolflareWalletAdapter
+  SolflareWalletAdapter,
+  TorusWalletAdapter
 } from '@solana/wallet-adapter-wallets';
 import Scene from './components/Scene';
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './styles/globalStyles';
+
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 function App() {
   const network = WalletAdapterNetwork.Devnet;
-  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+  const endpoint = 'https://api.devnet.solana.com';
+
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+    new TorusWalletAdapter()
+  ];
 
   return (
-    <ConnectionProvider endpoint={'https://api.devnet.solana.com'}>
+    <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          <div className="App">
-            <h1>The Solana Billboard</h1>
-            <WalletMultiButton />
-            <Scene />
-          </div>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <div className="App">
+              <header>
+                <h1>The Solana Billboard</h1>
+                <WalletMultiButton />
+              </header>
+              <Scene />
+            </div>
+          </ThemeProvider>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
@@ -36,4 +51,3 @@ function App() {
 }
 
 export default App;
-
